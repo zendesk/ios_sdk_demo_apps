6a77ed125783cb618f5ffeb9f911237d6a12cdb5
Barry Carroll <bcarroll@zendesk.com>
2016-06-01 13:29:49 +0100

    Fiat Lux

---
+++ ChatBadgeCount/ChatBadgeCount/FirstViewController.swift
@@ -0,0 +1,83 @@
+//
+//  FirstViewController.swift
+//  ChatBadgeCount
+//
+//  Created by Barry Carroll on 30/05/2016.
+//  Copyright © 2016 chatbadge. All rights reserved.
+//
+
+import UIKit
+import ZDCChat
+
+class FirstViewController: UIViewController, UITabBarControllerDelegate {
+
+    var currentMessageCount: NSInteger = 0
+    var badgeTabBarItem: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Grab a reference to the UITabBarItem which shows the badge count.
+        badgeTabBarItem = tabBarController?.tabBar.items?[1]
+        tabBarController?.delegate = self
+    }
+
+    /*
+        This method will calculate the number of unread messages and display them on a badge.
+    */
+    func chatEvent() {
+        
+        let newMessageCount = ZDCChat.instance().session.dataSource().agentMessageCount()
+        
+        var unreadCount: NSInteger = 0
+        
+        if (currentMessageCount != 0 && newMessageCount != 0) {
+            
+            if (newMessageCount > currentMessageCount) {
+                
+                unreadCount = newMessageCount - currentMessageCount
+                badgeTabBarItem.badgeValue = String(unreadCount)
+            }
+        }
+    }
+    
+    /*
+        When this appears we assume that the chat has been hidden. We then start to listen
+        for any incoming messages.
+    */
+    override func viewDidAppear(animated: Bool) {
+        
+        currentMessageCount = ZDCChat.instance().session.dataSource().agentMessageCount()
+        
+        ZDCChat.instance().session.dataSource().addObserver(
+            self,
+            forChatLogEvents: #selector(chatEvent)
+        )
+    }
+    
+    /*
+        When this disappears we assume that we're going back to the chat, so we can 
+        assume that we have read all of the unread messages
+    */
+    override func viewWillDisappear(animated: Bool) {
+        
+        // Reset the unread count and set the badge to nil so it stops showing
+        currentMessageCount = 0
+        badgeTabBarItem.badgeValue = nil
+        
+        // Removes the observer because we don't need it
+        ZDCChat.instance().session.dataSource().removeObserverForChatLogEvents(self)
+    }
+    
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        
+        // Don't allow the chat to be 'popped under'
+        if (tabBarController.selectedIndex == 1 && viewController.isKindOfClass(UINavigationController)) {
+            return false
+        }
+        
+        return true
+    }
+}
+
---
+++ ChatBadgeCount/README
@@ -0,0 +1,7 @@
+## ChatBadgeCount
+
+This project shows how to use the [Chat API](https://developer.zendesk.com/embeddables/docs/ios-chat-sdk/sessionapi) to show the number of unread chat messages on a tab.
+
+## Getting started
+
+Replace `{accountKey}` in `AppDelegate.swift` with your own account ID.
