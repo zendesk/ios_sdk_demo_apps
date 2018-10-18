711a67050fef1cbcd76e0f699a47a2e0b19cd760
Killian Smith <kismith@zendesk.com>
2018-10-18 09:31:27 +0100

    Update comments

--- AnswerBotSDKSample/AnswerBotSample/AppDelegate.swift
+++ AnswerBotSDKSample/AnswerBotSample/AppDelegate.swift
@@ -17,30 +17,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        /**
-         * Initialize the SDK with your Zendesk instance
-         * Get these details from your Zendesk dashboard: Admin -> Channels -> MobileSDK.
-         */
+
+        // Initialize the SDK with your Zendesk instance
+        // Get these details from your Zendesk dashboard: Admin -> Channels -> MobileSDK.
          Zendesk.initialize(appId: "appId",
                             clientId: "cleintId",
                             zendeskUrl: "zendeskUrl")
         
         Support.initialize(withZendesk: Zendesk.instance)
-
-        /**
-         * Set an identity (authentication).
-         * Set either Anonymous or JWT identity, as below:
-         */
-         let identity = Identity.createAnonymous()
-        // let identity = Identity.createJwt(token: "JWT User Identifier")
+        // Support is needed to hand off tickets from AB
         
+        // Set an identity for authentication.
+        // let identity = Identity.createAnonymous()
+        let identity = Identity.createJwt(token: "JWT_User_Identifier")
         Zendesk.instance?.setIdentity(identity)
-        /**
-         * Initialize the Answer Bot SDK using the Zendesk and Support singletons you just initialized
-         */
+        
+        // Initialize Answer Bot with instances of Zendesk and Support singletons
         guard let support = Support.instance else { return false }
         AnswerBot.initialize(withZendesk: Zendesk.instance, support: support)
+        
         return true
     }
 }
