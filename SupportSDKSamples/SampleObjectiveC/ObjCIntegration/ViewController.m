35f2f619acf074fb625323b0321afc3b3e484ed8
Killian Smith <kismith@zendesk.com>
2019-01-08 16:02:55 +0000

    Objective C integration

---
+++ SupportSDKSamples/SampleObjectiveC/ObjCIntegration/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  ObjCIntegration
+//
+
+#import "ViewController.h"
+#import <ZendeskSDK/ZendeskSDK.h>
+#import <ZendeskCoreSDK/ZendeskCoreSDK.h>
+#import <ZendeskProviderSDK/ZendeskProviderSDK.h>
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *helpCenterButton;
+@property (weak, nonatomic) IBOutlet UIButton *tickets;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)openHelpCenter:(id)sender {
+    ZDKHelpCenterUiConfiguration *hcConfig = [ZDKHelpCenterUiConfiguration new];
+    UIViewController *helpCenter = [ZDKHelpCenterUi buildHelpCenterOverviewWithConfigs:@[hcConfig]];
+//        [ZDKHelpCenterUi buildHelpCenterArticleWithArticleId:(nonnull NSString ]
+    [self.navigationController pushViewController:helpCenter animated:YES];
+
+}
+
+- (IBAction)openTickets:(id)sender {
+    UIViewController *tickets = [ZDKRequestUi buildRequestList];
+    [self.navigationController pushViewController:tickets animated:YES];
+}
+
+@end
---
+++ SupportSDKSamples/SampleObjectiveC/ObjCIntegration/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCIntegration
+//
+//  Created by Killian Smith on 29/05/2018.
+//  Copyright © 2018 Killian Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
