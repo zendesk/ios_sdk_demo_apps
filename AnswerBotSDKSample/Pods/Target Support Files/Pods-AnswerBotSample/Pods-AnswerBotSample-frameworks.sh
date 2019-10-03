2b10eba265eceb429476d5b85f22517bb1df8fae
Alan O'Connor <aloconnor@zendesk.com>
2019-10-03 13:42:11 +0100

    Updated Answer Bot SampleApp to use AnswerBot 1.0.2

--- AnswerBotSDKSample/Pods/Target Support Files/Pods-AnswerBotSample/Pods-AnswerBotSample-frameworks.sh
+++ AnswerBotSDKSample/Pods/Target Support Files/Pods-AnswerBotSample/Pods-AnswerBotSample-frameworks.sh
@@ -161,20 +161,20 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.0.1/AnswerBotProvidersSDK.framework"
-  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.0.1/AnswerBotSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/ZendeskCoreSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/ZendeskProviderSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/ZendeskSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/CommonUISDK.framework"
+  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.1/AnswerBotProvidersSDK.framework"
+  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.1/AnswerBotSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/ZendeskCoreSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/ZendeskProviderSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/ZendeskSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/CommonUISDK.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.0.1/AnswerBotProvidersSDK.framework"
-  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.0.1/AnswerBotSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/ZendeskCoreSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/ZendeskProviderSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/ZendeskSDK.framework"
-  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.0.1/CommonUISDK.framework"
+  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.1/AnswerBotProvidersSDK.framework"
+  install_framework "${PODS_ROOT}/AnswerBotSDK/AnswerBotSDK/5.1/AnswerBotSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/ZendeskCoreSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/ZendeskProviderSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/ZendeskSDK.framework"
+  install_framework "${PODS_ROOT}/ZendeskSDK/ZendeskSDK/5.1/CommonUISDK.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
