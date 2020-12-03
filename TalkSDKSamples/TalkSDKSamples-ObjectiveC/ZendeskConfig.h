6ea9d3dab0d6f150f453302a23f54576c8f5f84a
Michal Smaga <michal.smaga@getbase.com>
2020-12-03 14:10:37 +0100

    Add first sample in Objective-C

---
+++ TalkSDKSamples/TalkSDKSamples-ObjectiveC/ZendeskConfig.h
@@ -0,0 +1,17 @@
+//
+//  ZendeskConfig.h
+//  TalkSDKSamples-ObjectiveC
+//
+//  Created by Michał Smaga on 03/12/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString *const ZENZendeskAppID;
+extern NSString *const ZENZendeskClientID;
+extern NSString *const ZENZendeskURL;
+extern NSString *const ZENZendeskDigitalLine;
+
+NS_ASSUME_NONNULL_END
---
+++ TalkSDKSamples/TalkSDKSamples-ObjectiveC/ZendeskConfig.m
@@ -0,0 +1,13 @@
+//
+//  ZendeskConfig.m
+//  TalkSDKSamples-ObjectiveC
+//
+//  Created by Michał Smaga on 03/12/2020.
+//
+
+#import "ZendeskConfig.h"
+
+NSString *const ZENZendeskAppID = @"";
+NSString *const ZENZendeskClientID = @"";
+NSString *const ZENZendeskURL = @"";
+NSString *const ZENZendeskDigitalLine = @"";
