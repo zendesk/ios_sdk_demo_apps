8e06b7944132db0167e9a3b30bfe221d2905ec82
dengliwen <colin@corp.kucoin.com>
2021-05-19 19:32:16 +0800

    Add ZendeskSDKMessaging SDK demo

---
+++ ZendeskSDKMessagingDemo/ZendeskSDKMessagingDemo/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  ZendeskSDKMessagingDemo
+//
+//  Created by 邓利文 on 2021/5/19.
+//
+
+import UIKit
+import ZendeskSDKMessaging
+
+class ViewController: UIViewController {
+
+    private enum InitStatus {
+        case notReady
+        case initializing //initializing
+        case ready
+        case failed
+    }
+    
+    private var status: InitStatus = .notReady
+    private var messaging: ZendeskSDKMessaging.Messaging?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _initMessagingIfNeeded()
+    }
+
+    @IBAction func openSupportController(_ sender: Any) {
+        _initMessagingIfNeeded { (status) in
+            guard status == .ready, let vc = self.messaging?.messagingViewController() else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+    }
+    
+    private func _initMessagingIfNeeded(_ completion: ((_ status: InitStatus) -> Void)? = nil) {
+        if status == .ready || status == .initializing {
+            completion?(status)
+        } else {
+            status = .initializing
+            ZendeskSDKMessaging.Messaging.initialize(channelKey: "eyJzZXR0aW5nc191cmwiOiJodHRwczovL2t1Y29pbi56ZW5kZXNrLmNvbS9tb2JpbGVfc2RrX2FwaS9zZXR0aW5ncy8wMUYzUTI2RlhNVFo1MkZKSk45WDhYWkJOOC5qc29uIn0=") { (result) in
+                do {
+                    self.messaging = try result.get()
+                    self.status = .ready
+                    completion?(.ready)
+                } catch {
+                    self.status = .failed
+                    completion?(.failed)
+                }
+            }
+        }
+    }
+    
+}
+
