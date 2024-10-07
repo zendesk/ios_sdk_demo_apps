e63bc7f2c479b9ed422850b387f55b9627a8f42d
Jérémy Ben Abu <jeremy.benabu@zendesk.com>
2024-10-07 11:42:31 +0200

    Update sample apps for october release

--- ChatSDKSamples/ChatSDK_Auth/ChatSDK_Auth/ZendeskMessaging.swift
+++ ChatSDKSamples/ChatSDK_Auth/ChatSDK_Auth/ZendeskMessaging.swift
@@ -25,7 +25,7 @@ final class ZendeskMessaging: NSObject, JWTAuthenticator {
     }
 
     #warning("Please provide Chat account key")
-    let accountKey = "<#String#>"
+    let accountKey = "OXgeUeElrZ7hzQ96yrELV4xxlJC91awA"
 
     var authToken: String = "" {
         didSet {
